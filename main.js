db06914301a88d7601dbf5a3958202675220d529
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-09-20 04:58:15 +0600

    Request Response Model Works in node js

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
+// var http = require('http');
+
+
+// var server =http.createServer(function(req,res){
+//     res.end("Hello World!");
+// });
+
+// server.listen(8080);
+// console.log('server Run Success!');
+
 var http = require('http');
 
 
 var server =http.createServer(function(req,res){
-    res.end("Hello World!");
+    if(req.url=="/"){
+        res.writeHead(200,{'content-Type':'text/html'});
+        res.write('<h1>This is Home Page</h1>');
+        res.end();
+    }else if(req.url=="/about"){
+        res.writeHead(200,{'content-Type':'text/html'});
+        res.write('<h1>This is about Page</h1>');
+        res.end();
+    }else if(req.url=="/contact"){
+    res.writeHead(200,{'content-Type':'text/html'});
+    res.write('<h1>This is contact Page</h1>');
+    res.end();
+    }
 });
 
-server.listen(8080);
+server.listen(8085);
 console.log('server Run Success!');
