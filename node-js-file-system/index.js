662b23104c95d620baf86549e87b61bbfc7f8ce6
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-10-20 16:29:35 +0600

    live class 2 practices

---
+++ node-js-file-system/index.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+
+//write a file
+
+// async function writeFileSync(){
+//     try{
+//         const data = await fs.writeFileSync('sample.txt', 'hello 3');
+//         console.log('File created successfully');
+//     }
+//     catch(err){
+//         console.error(err);
+//     }
+// }
+
+// (async () => {
+//     await writeFileSync();
+// })();
+
+
+// async function readFileSync(){
+//     try{
+//         const data = await fs.readFileSync('sample.txt', 'utf8');
+//         console.log(data);
+//     }
+//     catch(err){
+//         console.error(err);
+//     }
+// }
+
+// (async () => {
+//     await readFileSync();
+// })();
+
+// async function renameSync(){
+//     try{
+//         const data = await fs.renameSync('sample2.txt', 'tex.txt');
+//         console.log('File created successfully');
+//     }
+//     catch(err){
+//         console.error(err);
+//     }
+// }
+
+// renameSyn();
+
+// async function deleteFileSync(){
+//     try{
+//         const data = await fs.unlinkSync('sample2.txt');
+//         console.log('File created successfully');
+//     }
+//     catch(err){
+//         console.error(err);
+//     }
+// }
+
+// (async () => {
+//     await deleteFileSync();
+// })();
+
+
+//Folder Manage
+
+async function CreateFolderSync(){
+    try{
+        const data = await fs.rmdirSync('sample2.txt');
+        console.log('File created successfully');
+    }
+    catch(err){
+        console.error(err);
+    }
+}
+
+CreateFolderSync();
+
+// const data = await fs.readdirSync('sample2.html');
+// const data = await fs.rmdirSync('sample2.html');
+// const data = await fs.renameFile('sample2.html', 'sample');
